ad475c21bfe7f90cbf176afb25befd9bed9e1082
huza <huzaifahasif3@gmail.com>
2025-12-14 18:11:38 +0700

    remove admin management

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -43,11 +43,6 @@ const navItems: NavItem[] = [
   },
   {
     icon: <UserCircleIcon />,
-    name: "Admins",
-    path: "/admin",
-  },
-  {
-    icon: <UserCircleIcon />,
     name: "User Profile",
     path: "/profile",
   },
